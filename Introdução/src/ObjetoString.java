48969199b36c3e1848f5bf3f8f04337096395c06
Andre Santos <andrefelipe_santos@outlook.com>
2023-11-03 20:20:36 -0300

    String e Arrays

---
+++ Introdução/src/ObjetoString.java
@@ -0,0 +1,8 @@
+public class ObjetoString {
+    public static void main(String[] args) {
+        String frase = "Estou aprendendo Java";
+        String numeros = "09890";
+
+        System.out.println(frase + numeros);
+    }
+}
